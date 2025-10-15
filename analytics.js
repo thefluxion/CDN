b6dc0f43b047e5b93211e8a20c8058cd55c02702
fluxion <129456340+thefluxion@users.noreply.github.com>
2025-10-15 15:39:47 +0900

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,18 @@
+        (async () => {
+    try {
+      const url = window.location.href;
+      const res = await fetch("http://v1.mathkits.org/api/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          url
+        })
+      });
+      const data = await res.json();
+      console.log("URL submission result:", data);
+    } catch (err) {
+      console.error("Failed to send URL to server:", err);
+    }
+  })();
